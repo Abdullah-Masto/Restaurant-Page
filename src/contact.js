19d959f53b1f0ab34c387f14fc1b13686dbbd902
Abdullah-Masto <abdullahmasto77@gmail.com>
2023-10-15 22:08:32 +0300

    add menu and contact sections

--- src/contact.js
+++ src/contact.js
@@ -1,3 +1,33 @@
-function contact() {}
+const content = document.querySelector("#content");
+const phone = document.createElement("span");
+const mail = document.createElement("span");
+const phoneIcon = document.createElement("i");
+const mailIcon = document.createElement("i");
+const phoneText = document.createElement("p");
+const mailText = document.createElement("p");
+
+function contact() {
+  if (phoneIcon.classList.length == 0) {
+    phoneIcon.classList.add("fa-solid");
+    phoneIcon.classList.add("fa-phone");
+    phone.appendChild(phoneIcon);
+  }
+  phoneText.textContent = "+123456789";
+  phone.appendChild(phoneText);
+
+  if (mailIcon.classList.length == 0) {
+    mailIcon.classList.add("fa-solid");
+    mailIcon.classList.add("fa-envelope");
+    mail.appendChild(mailIcon);
+  }
+  mailText.textContent = "Gusteau's@fake-email.com";
+  mail.appendChild(mailText);
+
+  phone.classList.add("info");
+  mail.classList.add("info");
+
+  content.appendChild(phone);
+  content.appendChild(mail);
+}
 
 export default contact;
--- src/menu.js
+++ src/menu.js
@@ -1,3 +1,69 @@
-function menu() {}
+import cheeseBurger from "./images/cheese-burger.png";
+import hamburger from "./images/hamburger.png";
+import chickenBurger from "./images/chicken-burger.png";
+import shawarma from "./images/shawarma.png";
+import frenchFries from "./images/french-fries.png";
+import salad from "./images/salad.png";
+
+const content = document.querySelector("#content");
+const gridContainer = document.createElement("div");
+const link = document.createElement("a");
+const plates = [];
+for (let i = 0; i < 6; i++) {
+  let plate = document.createElement("div");
+  let image = document.createElement("img");
+  let title = document.createElement("h3");
+  let price = document.createElement("p");
+
+  plate.classList.add("plate");
+  title.classList.add("title");
+  price.classList.add("price");
+
+  plate.appendChild(image);
+  plate.appendChild(title);
+  plate.appendChild(price);
+
+  plates.push(plate);
+}
+
+function menu() {
+  plates[0].getElementsByTagName("img")[0].src = cheeseBurger;
+  plates[0].getElementsByClassName("title")[0].textContent = "Cheese Burger";
+  plates[0].getElementsByClassName("price")[0].textContent = "$2.99";
+
+  plates[1].getElementsByTagName("img")[0].src = hamburger;
+  plates[1].getElementsByClassName("title")[0].textContent = "HamBurger";
+  plates[1].getElementsByClassName("price")[0].textContent = "$2.49";
+
+  plates[2].getElementsByTagName("img")[0].src = chickenBurger;
+  plates[2].getElementsByClassName("title")[0].textContent = "Chicken Burger";
+  plates[2].getElementsByClassName("price")[0].textContent = "$1.99";
+
+  plates[3].getElementsByTagName("img")[0].src = shawarma;
+  plates[3].getElementsByClassName("title")[0].textContent = "Shawarma";
+  plates[3].getElementsByClassName("price")[0].textContent = "$1.49";
+
+  plates[4].getElementsByTagName("img")[0].src = frenchFries;
+  plates[4].getElementsByClassName("title")[0].textContent = "French Fries";
+  plates[4].getElementsByClassName("price")[0].textContent = "$0.99";
+
+  plates[5].getElementsByTagName("img")[0].src = salad;
+  plates[5].getElementsByClassName("title")[0].textContent = "Salad";
+  plates[5].getElementsByClassName("price")[0].textContent = "$0.99";
+
+  plates.forEach((plate) => {
+    gridContainer.appendChild(plate);
+  });
+
+  gridContainer.id = "grid-container";
+
+  link.textContent = "images from pixabay.com";
+  link.target = "_blank";
+  link.href = "https://pixabay.com";
+  link.id = "attribution";
+
+  content.appendChild(gridContainer);
+  content.appendChild(link);
+}
 
 export default menu;
